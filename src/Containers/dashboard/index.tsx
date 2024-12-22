298014115fe41295b9e6da2248dfa738310bd7c2
waliaAnurag <walianura5@gmail.com>
2024-12-22 16:12:01 +0530

    Minor changes

--- src/Containers/dashboard/index.tsx
+++ src/Containers/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Lottie from "react-lottie";
-import {CardData} from "./utils"
+import { CardData } from "./utils"
 import ErrorBounday from "../../Components/ErrorBoundary";
 
 function Dashboard() {
@@ -9,46 +9,46 @@ function Dashboard() {
   const navigate = useNavigate();
 
 
-  function navigateTo(screenPath:string){
+  function navigateTo(screenPath: string) {
     navigate(screenPath)
-}
+  }
   return (
     <ErrorBounday useDefaultFallback={true}>
-    <div className="w-full h-screen p-[11px] bg-landingPage text-headingFontColor">
-      <div className="h-[95px]"/>
-      <header className="flex flex-row justify-between gap-2">
-        {
-          CardData.map((item: any,index:number)=>{
-            return(
-              <section key={index} className={`flex justify-between cursor-pointer h-auto bg-headingFontColor p-3 rounded-md border-solid border-2 border-indigo-500/100 w-[40%]`} onClick={() => navigateTo('/dashboard/info')}>
-                <section className="self-center w-[40%]">
-                  <h2 className="text-lg text-landingPage font-display font-bold leading-normal">
-                    {t(item.cardHeading)}
-                  </h2>
-                  <br />
-                  <span className="text-lg text-landingPage font-display font-bold leading-normal">{item.totalDiscovered}</span>
-                  <br />
-                  <span className="text-lg text-landingPage font-display font-bold leading-normal">
-                    {item.untill} <br/> {item.date}
-                  </span>
+      <div className="w-full h-screen p-[11px] bg-landingPage text-headingFontColor">
+        <div className="h-[95px]" />
+        <header className="flex flex-row justify-between gap-2">
+          {
+            CardData.map((item: any, index: number) => {
+              return (
+                <section key={index} className={`flex justify-between cursor-pointer h-auto bg-headingFontColor p-3 rounded-md border-solid border-2 border-indigo-500/100 w-[40%]`} onClick={() => navigateTo('/dashboard/info')}>
+                  <section className="self-center w-[40%]">
+                    <h2 className="text-lg text-landingPage font-display font-bold leading-normal">
+                      {t(item.cardHeading)}
+                    </h2>
+                    <br />
+                    <span className="text-lg text-landingPage font-display font-bold leading-normal">{item.totalDiscovered}</span>
+                    <br />
+                    <span className="text-lg text-landingPage font-display font-bold leading-normal">
+                      {item.untill} <br /> {item.date}
+                    </span>
+                  </section>
+                  <section className="w-[65%]">
+                    <Lottie
+                      options={item.defaultOption}
+                      height={'80%'}
+                      width={'100%'}
+                    />
+                  </section>
                 </section>
-                <section className="w-[65%]">
-                  <Lottie
-                    options={item.defaultOption}
-                    height={'80%'}
-                    width={'100%'}
-                  />
-                </section>
-              </section>
-            )
-          })
-        }
-      
-      </header>
-      <div className="mt-[12px] p-[12px] bg-headingFontColor rounded-md border-solid border-2 border-indigo-500/100">
-      
+              )
+            })
+          }
+
+        </header>
+        <div className="mt-[12px] p-[12px] bg-headingFontColor rounded-md border-solid border-2 border-indigo-500/100">
+
+        </div>
       </div>
-    </div>
     </ErrorBounday>
   )
 }
